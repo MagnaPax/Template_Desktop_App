e3739905490918927232d52f33eb65f677d0e303
ChunHee Han <chunhee.han@outlook.com>
2026-03-03 16:47:59 +0900

    refactor(vm-main-window): 의존성 주입받음

--- view_models/main_window_vm.py
+++ view_models/main_window_vm.py
@@ -4,6 +4,9 @@ class MainViewModel(BaseViewModel):
     """
     메인 윈도우의 비즈니스 로직과 상태를 관리하는 ViewModel
     """
-    def __init__(self):
+    def __init__(self, system_manager=None):
         super().__init__()
+        # 상태 관리를 담당하는 매니저를 외부(main.py)로부터 주입받아 저장한다 (Has-A 관계)
+        self.system_manager = system_manager
+        
         self.log_info("MainViewModel 초기화됨")
